3b856591a893f8b72bd9c36597ba29332b07e190
Sorin Chis <kish.sorin@yahoo.com>
2019-04-18 14:46:02 +0300

    added endpoind for stripe api

--- server/index.js
+++ server/index.js
@@ -1,13 +1,16 @@
+require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
-dotenv.config();
 const app = express();
 app.use(cors());
+app.use(express.json());
+
 const configureRoutes = require("./routes");
-configureRoutes(app);
-app.listen(8000, error => {
+app.use(configureRoutes);
+app.listen(5000, error => {
   if (error) throw error;
-  console.log("Server running on port " + 8000);
+  console.log("Server running on port " + 5000);
 });
