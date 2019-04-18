1109b4d31e175a44b18fdf9c2024f5ad7e01692e
Sorin Chis <kish.sorin@yahoo.com>
2019-04-18 14:20:59 +0300

    added text on cards

--- client/src/components/StripeComponent.jsx
+++ client/src/components/StripeComponent.jsx
@@ -26,14 +26,14 @@ const stripeComponent = () => {
 
   return (
     <StripeCheckout
-      label="Go Premium" //Component button text
-      name="Business LLC" //Modal Header
-      description="Upgrade to a premium account today."
+      label="Go and Pay" //Component button text
+      name="Lambda School" //Modal Header
+      description="My frst try thist Stripe"
       panelLabel="Go Premium" //Submit button in modal
       amount={999} //Amount in cents $9.99
       token={onToken}
       stripeKey={publisherKey}
-      image="https://www.vidhub.co" //Pop-in header image
+      //image="https://www.vidhub.co" //Pop-in header image
       billingAddress={false}
     />
   );
